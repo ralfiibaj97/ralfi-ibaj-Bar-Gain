bedc340237c4ce402ffeebb981b606ef740c588e
Ralfi <ri564@nyu.edu>
2024-09-26 22:42:08 -0400

    Update header section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./Components/Header/Header";
+import Hero from "./Components/Hero/Hero";
 
 function App() {
   return (
     <>
       <Header />
+      <Hero />
     </>
   );
 }
