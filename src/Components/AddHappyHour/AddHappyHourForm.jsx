4a0f4951977eefc631982e29fbcf3746d1e39696
Ralfi <ri564@nyu.edu>
2024-09-29 19:11:53 -0400

    Fix the form styling

--- src/Components/AddHappyHour/AddHappyHourForm.jsx
+++ src/Components/AddHappyHour/AddHappyHourForm.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useNavigate, useParams } from "react-router-dom";
+import { useState } from "react";
 import "./AddHappyHourForm.scss";
 
 const AddHappyHourForm = () => {
--- src/Components/BarDetails/BarDetails.jsx
+++ src/Components/BarDetails/BarDetails.jsx
@@ -1,7 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
-
+import { useParams } from "react-router-dom";
 import "./BarDetails.scss";
 
 const BarDetails = () => {
